8ffcf6277548071a85d418ab58a035946d90e66f
ivan cholakov <icholakov1@gmail.com>
2024-05-26 19:35:56 +0300

    fix: env vars test

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// @ts-ignore
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
